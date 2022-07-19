084e40e99b465f35a8e89ffbe0c1c231608681e5
Elmira Ilyasova <capricorn_26@mail.ru>
2022-07-19 23:14:18 +0700

    Add card hover behavior

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -9,20 +9,35 @@ interface ProductCardProps {
 const ProductCard = ({product}: ProductCardProps): JSX.Element => {
   return (
     <div className="product-card">
-      <div className="product-card__tags">
-        <div className="product-card__tag product-card__tag--hit">Хит</div>
-        <div className="product-card__tag product-card__tag--discount">Скидка</div>
-        <div className="product-card__tag product-card__tag--promotion">Акция</div>
-      </div>
+      <div className="product-card__wrapper">
+        <div className="product-card__tags">
+          <div className="product-card__tag product-card__tag--hit">Хит</div>
+          <div className="product-card__tag product-card__tag--discount">Скидка</div>
+          <div className="product-card__tag product-card__tag--promotion">Акция</div>
+        </div>
 
-      <div className="product-card__image-wrapper">
-        <img className="product-card__image" src={require(`../../img/${product.image}`)} alt="Изображение товара" />
-      </div>
+        <button className="product-cart__favorite-button">В избранное</button>
 
-      <div className="product-card__gost">{product.gost}</div>
-      <h5 className="product-card__title">{product.title}</h5>
-      <p className="product-card__price">{product.price} руб.</p>
-      
+        <div className="product-card__image-wrapper">
+          <img className="product-card__image" src={require(`../../img/${product.image}`)} alt="Изображение товара" />
+        </div>
+
+        <div className="product-card__gost">{product.gost}</div>
+        <h5 className="product-card__title">{product.title}</h5>
+
+        <div className="product-card__purchase-block">
+          <p className="product-card__price">{product.price} руб.</p>
+
+          <div className="product-card__add-block">
+            <button className="product-card__increment-button">+</button>
+            <div className="product-card__added-count">3</div>
+            <button className="product-card__decrement-button">-</button>
+          </div>
+        </div>
+
+        <button className="product-card__add-button">В корзину</button>
+        <button className="product-card__details-button">Подробнее</button>
+      </div>
     </div>
   );
 };
