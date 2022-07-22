c519b1f353468b736450fa1a66bea363bc042552
Elmira Ilyasova <capricorn_26@mail.ru>
2022-07-22 19:39:41 +0700

    Add product filtration

--- src/components/product-cards/product-cards.tsx
+++ src/components/product-cards/product-cards.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
+import { useSelector } from "react-redux";
 
+import { RootState } from "../../store/store";
 import { Products } from "../../types";
 
 import ProductCard from "../product-card/product-card";
@@ -9,9 +11,34 @@ interface ProductCardsProps {
 }
 
 const ProductCards = ({products}: ProductCardsProps): JSX.Element => {
+  const priceFilter = useSelector((state: RootState) => state.filter.priceFilter);
+  const typeFilter = useSelector((state: RootState) => state.filter.typeFilter);
+  const gostFilter = useSelector((state: RootState) => state.filter.gostFilter);
+
+  const filterProducts = (products: Products) => {
+    if (!priceFilter && !typeFilter.length && !gostFilter.length) {
+      return products;
+    }
+
+    return products.filter(product => {
+      if (priceFilter && (product.price < priceFilter.min || product.price > priceFilter.max)) {
+        return false;
+      }
+
+      if (typeFilter.length && !typeFilter.includes(product.type)) {
+        return false;
+      }
+
+      if (gostFilter.length && !gostFilter.includes(product.gost)) {
+        return false;
+      }
+      return true;
+    });
+  };
+
   return (
     <div className="product-cards">
-      {products.map((product, i) => <ProductCard key={i} product={product} />)}
+      {filterProducts(products).map((product, i) => <ProductCard key={i} product={product} />)}
     </div>
   );
 };
