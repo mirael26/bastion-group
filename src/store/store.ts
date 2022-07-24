de22e6fa5bd13b9132ded9f06581998c3001c039
Elmira Ilyasova <capricorn_26@mail.ru>
2022-07-25 01:51:32 +0700

    Add localStorage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,37 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
 import { cartReducer } from './reducers/cart-reducer';
 import { dataReducer } from './reducers/data-reducer';
 import { filterReducer } from './reducers/filter-reducer';
 
+const saveToLocalStorage = (state: RootState) => {
+  try {
+    localStorage.setItem('state', JSON.stringify(state));
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const stateStr = localStorage.getItem('state');
+    return stateStr ? JSON.parse(stateStr) : undefined;
+  } catch (e) {
+    console.error(e);
+    return undefined;
+  }
+};
+
 const store = configureStore({
   reducer: {data: dataReducer, cart: cartReducer, filter: filterReducer},
+  preloadedState: loadFromLocalStorage(),
   devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
+});
 
 export default store;
